59183ba2c502510dd20a6454c86884c703ac1d54
Razmik Movsisyan <razmikmovsisyan@icloud.com>
2025-01-13 21:57:03 +0000

    Create main function to run the expense tracker workflow

--- run.py
+++ run.py
@@ -2,3 +2,15 @@ from expense import Expense
 
 EXPENSE_FILE_PATH = "expenses.csv"  
 BUDGET = 2000
+
+def main():
+    print(f"Running Expense Tracker!")
+    
+    # Get user to input an expense
+    expense = get_user_expense()
+
+    # Write the expenses to a file
+    save_expense_to_file(expense, EXPENSE_FILE_PATH)
+
+    # Read file and summarize all expenses
+    summarize_expenses(EXPENSE_FILE_PATH, BUDGET)
