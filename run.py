fbc133c6dcf2615432ef70b91d59e4d1a2b51199
Razmik Movsisyan <razmikmovsisyan@icloud.com>
2025-01-13 22:32:21 +0000

    Add function to read expenses and summarize by category, also calculate remaining budget

--- run.py
+++ run.py
@@ -57,3 +57,31 @@ def save_expense_to_file(expense: Expense, expense_file_path):
     print(f"Saving User Expense: {expense} to {expense_file_path}")
     with open(expense_file_path, "a") as f:
         f.write(f"{expense.name},{expense.amount},{expense.category}\n")
+
+def summarize_expenses(expense_file_path, budget):
+    print(f"Summarizing User Expense")
+    
+    expenses = []  # empty list to hold expense objects
+    with open(expense_file_path, "r") as f:
+        for line in f:
+            expense_name, expense_amount, expense_category = line.strip().split(",")
+            line_expense = Expense(
+                name=expense_name,
+                amount=float(expense_amount),
+                category=expense_category,
+            )
+            expenses.append(line_expense)
+
+    amount_by_category = {}  # a dictionary to store the sum of expenses depending on which category
+    for expense in expenses:
+        amount_by_category[expense.category] = amount_by_category.get(expense.category, 0) + expense.amount
+
+    print("Expenses By Category:")
+    for key, amount in amount_by_category.items():
+        print(f"  {key}: ${amount:.2f}")
+
+    total_spent = sum(expense.amount for expense in expenses)
+    print(f"Total Spent: ${total_spent:.2f}")
+
+    remaining_budget = budget - total_spent
+    print(f"Budget Remaining: ${remaining_budget:.2f}")
